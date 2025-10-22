5a1d9efaa9b54a60bc7175d34fa7515696b31367
Logan Nelson <Lnelson0915@gmail.com>
2025-10-21 19:08:53 -0700

    Add ApplicationUser class extending IdentityUser
    Introduced a new `ApplicationUser` class in the `GameBox.Models`
    namespace, inheriting from `IdentityUser` to allow for future
    customizations of the user entity. Added `using` directives for
    `Microsoft.AspNetCore.Identity` and `System.Runtime.CompilerServices`.
    The class is currently empty and serves as a placeholder for
    extending user functionality in the application.

---
+++ GameBox/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Runtime.CompilerServices;
+
+namespace GameBox.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
