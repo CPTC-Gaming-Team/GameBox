f6cd246c9f86e0df3c4f180f596869339d7ae8bc
Logan Nelson <Lnelson0915@gmail.com>
2025-10-21 20:02:38 -0700

    Integrate ASP.NET Core Identity
    Added Identity Framework support by updating `ApplicationDbContext` to inherit from `IdentityDbContext<ApplicationUser>`. Configured Identity services in `Program.cs` and added a `DefaultConnection` string to `appsettings.json`. Updated `GameBox.csproj` with required NuGet packages for Identity and Entity Framework Core.
    
    Created an initial migration (`20251022025756_InitializeIdentity`) to define the database schema for Identity, including tables for users, roles, claims, and tokens. Generated a model snapshot to represent the current database state.

---
+++ GameBox/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GameBox.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace GameBox.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        // Track Users Listings
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>()
+                .HasIndex(m => m.Id)
+                .IsUnique();
+        }
+    }
+}
