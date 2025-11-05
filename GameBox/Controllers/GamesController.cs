529750225868200478bc8b34687f066b610f3039
Logan Nelson <Lnelson0915@gmail.com>
2025-11-04 21:09:09 -0800

    Add GameModel with CRUD, migrations, and Razor views
    Introduce GameModel entity with Creator relationship to track
    game creators. Add GamesController for CRUD operations and
    corresponding Razor views (Create, Edit, Delete, Details, Index).
    Include database migrations for schema updates, adding CreatorId
    to GameModels and establishing foreign key constraints. Add
    GameListViewModel for paginated game listings. Downgrade target
    framework to .NET 9.0 and adjust package versions.

---
+++ GameBox/Controllers/GamesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GameBox.Data;
+using GameBox.Models;
+
+namespace GameBox.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.GameModels.Include(g => g.Creator);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gameModel = await _context.GameModels
+                .Include(g => g.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gameModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(gameModel);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Rating,CreatorId")] GameModel gameModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gameModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", gameModel.CreatorId);
+            return View(gameModel);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gameModel = await _context.GameModels.FindAsync(id);
+            if (gameModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", gameModel.CreatorId);
+            return View(gameModel);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Rating,CreatorId")] GameModel gameModel)
+        {
+            if (id != gameModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gameModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameModelExists(gameModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", gameModel.CreatorId);
+            return View(gameModel);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gameModel = await _context.GameModels
+                .Include(g => g.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gameModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(gameModel);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gameModel = await _context.GameModels.FindAsync(id);
+            if (gameModel != null)
+            {
+                _context.GameModels.Remove(gameModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameModelExists(int id)
+        {
+            return _context.GameModels.Any(e => e.Id == id);
+        }
+    }
+}
--- GameBox/Models/GameModel.cs
+++ GameBox/Models/GameModel.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using GameBox.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading;
 
 namespace GameBox.Models
 {
+
     public class GameModel
     {
         [Key]
@@ -17,5 +20,21 @@ namespace GameBox.Models
 
         [Range(1, 10, ErrorMessage = "Ratings must be between 1 and 10")]
         public int Rating { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public required string CreatorId { get; set; }
+        public required ApplicationUser Creator { get; set; }
+
     }
+}
+
+public class GameListViewModel
+{
+    public required IEnumerable<GameModel> Games { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public string? SearchTerm { get; set; }
+
 }
