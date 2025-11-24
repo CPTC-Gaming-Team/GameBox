eff8363440f6d9cc79721758cf9c9b784aa57c48
ToastedToast0090 <toastedtoast0090@gmail.com>
2025-11-23 20:42:35 -0800

    Add social and game tracking properties to ApplicationUser
    Added `Followers` and `Following` properties to track user relationships.
    Added `WatchedGames` property to track games the user is watching.
    Initialized all new properties with empty lists to prevent null values.

--- GameBox/Models/ApplicationUser.cs
+++ GameBox/Models/ApplicationUser.cs
@@ -5,6 +5,13 @@ namespace GameBox.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        // user who follow me
         public ICollection<ApplicationUser> Followers { get; set; } = new List<ApplicationUser>();
+
+        // user who I follow
+        public ICollection<ApplicationUser> Following { get; set; } = new List<ApplicationUser>();
+
+        // games I watch
+        public ICollection<GameModel> WatchedGames { get; set; } = new List<GameModel>();
     }
 }
